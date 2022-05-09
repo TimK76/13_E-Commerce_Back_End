02913fd1fc0b5cf62292bd430931530bd708ec19
TimK76 <haadelphos2001@gmail.com>
2022-05-09 15:46:13 -0400

    associations made with some questions

--- models/index.js
+++ models/index.js
@@ -3,15 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { tableName } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+// Come back to check the through/as/fk parts
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'tag_id' 
+})
 // Tags belongToMany Products (through ProductTag)
-
+// Come back to check the through/as/fk parts 
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  as: 'tagged_products',
+  foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,
